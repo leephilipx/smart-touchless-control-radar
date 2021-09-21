7264b58944e8db49d73fffad18552e1b24364d5f
Lee Wai Yeong <waiyeonglee@gmail.com>
2021-09-21 16:43:36 +0800

    -

--- gui-dip-demo/daq_menu.py
+++ gui-dip-demo/daq_menu.py
@@ -15,17 +15,30 @@ class DataAcquisiton:
     def create_widgets(self, mainframe):
         filename = StringVar()
         filename_entry = ttk.Entry(mainframe, width=7, textvariable=filename)
-        filename_entry.grid(column=2, row=2, sticky=(W, E))
-        filename_entry.focus()
+        filename_entry.grid(row=3, column=1, sticky=(W, E))
+
+        counter = StringVar()
+        counter_entry = ttk.Entry(mainframe, width=3, textvariable=counter)
+        counter_entry.grid(row=3, column=5, sticky=(W, E))
         
-        ttk.Button(mainframe, text="Start Measurement", command=self.loadgui).grid(column=2, row=1, sticky=W)
-        ttk.Label(mainframe, text="Filename").grid(column=3, row=2, sticky=(N,W))
-        ttk.Button(mainframe, text="Next", command=self.loadgui).grid(column=1, row=3, sticky=W)
-        ttk.Button(mainframe, text="Stop", command=self.loadgui).grid(column=3, row=3, sticky=E)
+        ttk.Button(mainframe, text="Start Measurement", command=self.loadgui).grid(row=1, column=3)
+        ttk.Button(mainframe, text="Next", command=self.loadgui).grid(row=2, column=1, sticky=W)
+        ttk.Button(mainframe, text="Discard", command=self.loadgui).grid(row=2, column=3)
+        ttk.Button(mainframe, text="Stop", command=self.loadgui).grid(row=2, column=5)
+        ttk.Label(mainframe, text="Filename").grid(row=3, column=2, sticky=(N,W))
+        ttk.Label(mainframe, text="Counter").grid(row=3, column=6, sticky=(N,W))
+        ttk.Separator(mainframe, orient=HORIZONTAL).grid(row=4, columnspan=7, sticky=(E,W))
+        frame2 = ttk.Labelframe(mainframe, text='Magnitude plot', height=250, width=500)
+        frame2.grid(row=5, column=1, columnspan=6, sticky=(N,W))
+        ttk.Separator(mainframe, orient=HORIZONTAL).grid(row=9, columnspan=7, sticky=(E,W))
+        frame3 = ttk.Labelframe(mainframe, text='Config info', height=250, width=300)
+        frame3.grid(row=10, column=1, columnspan=6, sticky=(N,W))
+        
 
         for child in mainframe.winfo_children(): 
             child.grid_configure(padx=5, pady=5)
         
+        filename_entry.focus()
         root.bind("<Return>", self.loadgui)    
 
     def loadgui(self, *args):
