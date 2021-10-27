ce1c40dfba84da171cbbae290a155a1d7cc81511
Lee Wai Yeong <waiyeonglee@gmail.com>
2021-10-27 14:30:08 +0800

    stft&mag

--- final-pipeline/master/ml.py
+++ final-pipeline/master/ml.py
@@ -149,13 +149,14 @@ class DeepLearningModel():
     
 if __name__ == "__main__":
     # Get the data and preprocess it
-    deep = False
+    deep = True
     import radar, preprocess
     X, Y, class_labels = radar.getTrainData(source_dir='2021_10_20_data_new_gestures')
     # radar.cache('save', X, Y, class_labels)
     # X, Y, class_labels = radar.cache('load')
     print(X.shape, Y.shape, class_labels)
-    X_features = preprocess.get_batch(X, mode='stft')
+    # X_features = preprocess.get_batch(X, mode='stft')
+    X_features = preprocess.get_magnitude(X)
 
     if deep:
         X_input = preprocess.reshape_features(X_features, 'dl')
@@ -168,7 +169,7 @@ if __name__ == "__main__":
         callbacks = model.instantiate_callbacks()
         model.initialise_model(X_train, y_train_one_hot)
         train_acc, train_auc = model.train_batch(X_train, y_train_one_hot, validation_data = (X_test, y_test_one_hot), epochs=20, batch_size=8, callbacks=callbacks)
-        model.save_model('stft-run1.h5')
+        model.save_model('mag-run1.h5')
         test_acc, test_auc, y_preds = model.evaluate_batch(X_test, y_test_one_hot)
         print('Train-Test Acc =', round(train_acc, 5), round(test_acc, 5))
         print('Train-Test AUC =', round(train_auc, 5), round(test_auc, 5))
