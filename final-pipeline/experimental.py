d6233e77883f06165ba7ea31eec0aa3b1aa10593
Philip Lee <leephiliphy@gmail.com>
2021-10-31 02:07:58 +0800

    centering code experimental

--- final-pipeline/experimental.py
+++ final-pipeline/experimental.py
@@ -3,6 +3,17 @@ from time import sleep
 import numpy as np
 import argparse
 
+
+def experimental_centering(radar_data, consensus_buffer):
+
+    radar_mag = np.abs(np.squeeze(radar_data)).T
+    radar_mag = radar_mag / np.max(radar_mag)
+    x_coords = np.unique(np.where(radar_mag>0.4)[1])
+    x_center = np.round(np.mean(x_coords), decimals=0).astype(int)
+
+    return np.clip(x_center, 32+consensus_buffer, 48-consensus_buffer)
+
+
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='DIP E047: Final Real-time Gesture Prediction')
@@ -17,11 +28,10 @@ if __name__ == "__main__":
     radarSensor.connect_serial('/dev/ttyUSB0' if args.rpi else port)
     radarSensor.start_session()
     
-    X_frame = np.zeros((1, X_shape[1], X_shape[2]), dtype=np.complex)
+    X_frame = np.zeros((1, 80, X_shape[2]), dtype=np.complex)
     frame_buffer = 0
-    y_preds_buffer = np.zeros((len(class_labels), ))
     consensus_buffer = 2
-    confidence_threshold = 0.7
+    y_probs_buffer = np.zeros((len(class_labels), ))
     np.set_printoptions(suppress=True, precision=3)
 
     while True:
@@ -31,19 +41,22 @@ if __name__ == "__main__":
             X_frame[:, -1, :] = np.expand_dims(radarSensor.get_next(), axis=0)
             frame_buffer += 1
             
-            if frame_buffer >= (80-consensus_buffer):
-                # X_input = preprocess.get_magnitude(X_frame)
-                X_input = preprocess.get_batch(X_frame, mode='stft')
-                X_input = preprocess.reshape_features(X_input, type='dl')
-                y_probs = model.predict_proba(X_input)
-                y_preds = np.argmax(y_probs)
-                y_preds_buffer[y_preds] = y_preds_buffer[y_preds] + 1
+            if frame_buffer == 80:
 
-            if frame_buffer == (80+consensus_buffer):
-                frame_buffer = consensus_buffer
-                y_consensus = np.argmax(y_preds_buffer)
-                print(y_consensus, class_labels[y_consensus], np.squeeze(y_preds_buffer))
-                y_preds_buffer = np.zeros((len(class_labels), ))
+                x_center = experimental_centering(X_frame, consensus_buffer)
+
+                for offset in range(-consensus_buffer, consensus_buffer+1):
+                    X_input = X_frame[:, x_center+offset-32:x_center+offset+32, :]
+                    # X_input = preprocess.get_magnitude(X_input)
+                    X_input = preprocess.get_batch(X_input, mode='stft')
+                    X_input = preprocess.reshape_features(X_input, type='dl')
+                    y_probs = model.predict_proba(X_input)
+                    y_probs_buffer = y_probs_buffer + y_probs
+
+                frame_buffer = 0
+                y_consensus = np.argmax(y_probs_buffer)
+                print(f'center={x_center};  {y_consensus} {class_labels[y_consensus]}\t', np.squeeze(y_probs_buffer))
+                y_probs_buffer = np.zeros((len(class_labels), ))
 
         except KeyboardInterrupt:
             print('>> KeyboardInterrupt caught! Exiting ...')
