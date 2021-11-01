25c5ca7b2b6a3d0d48a13a0c2d9e1b5ff5ede2e1
Philip Lee <leephiliphy@gmail.com>
2021-11-01 12:22:41 +0800

    mag tflite

--- final-pipeline/experimental-tflite.py
+++ final-pipeline/experimental-tflite.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     parser.add_argument('-rpi', '--rpi', action='store_true', help='Raspberry Pi mode to fix port')
     args = parser.parse_args()
 
-    model = ml.DeepLearningModel(model_path='stft-run2.tflite')
+    model = ml.DeepLearningModel(model_path='mag-run1.tflite')
     X_shape, Y_shape, class_labels = radar.getDatasetInfo(source_dir='2021_10_20_data_new_gestures')
 
     radarSensor = radar.AcconeerSensorLive(config_path='sensor_configs_final.json')
@@ -47,8 +47,8 @@ if __name__ == "__main__":
 
                 for offset in range(-consensus_buffer, consensus_buffer+1):
                     X_input = X_frame[:, x_center+offset-32:x_center+offset+32, :]
-                    # X_input = preprocess.get_magnitude(X_input)
-                    X_input = preprocess.get_batch(X_input, mode='stft')
+                    X_input = preprocess.get_magnitude(X_input)
+                    # X_input = preprocess.get_batch(X_input, mode='stft')
                     X_input = preprocess.reshape_features(X_input, type='dl')
                     y_probs = model.predict_tflite(X_input)
                     y_probs_buffer = y_probs_buffer + y_probs
