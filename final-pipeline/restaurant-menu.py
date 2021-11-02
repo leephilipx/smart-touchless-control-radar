6dec516cd37525019277f53eb979178b5e7e6d04
Philip Lee <leephiliphy@gmail.com>
2021-11-02 15:36:49 +0800

    menu frontend

--- final-pipeline/restaurant-menu.py
+++ final-pipeline/restaurant-menu.py
@@ -1,58 +1,70 @@
 from tkinter import *
 import tkinter as tk
 import os
-import keyboard
-from PIL import ImageTk, Image
 
-root_path = os.path.dirname(__file__)
-path = os.path.join(root_path, 'ui_elements')
+class Menu:
+
+    def __init__(self, root):
+        self.mainframe = Frame(root)
+        root.title("Restaurant Menu")
+        root.geometry("800x600")
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe.pack(expand=True, fill='both', anchor='center')
+        self.root_path = os.path.dirname(__file__)
+        self.path = os.path.join(self.root_path, 'ui_elements')
+        self.create_widgets()
+        self.binding_events(root)
+        self.selection_counter = 0
+        self.borders[0].config(bg='blue')
+    
+
+    def create_widgets(self):
+
+        self.menu_max = 4
+        self.images = [None] * self.menu_max
+        self.borders = [None] * self.menu_max
+        self.buttons = [None] * self.menu_max
+        self.food_labels = [None] * self.menu_max
+        self.desc_labels = [None] * self.menu_max
+        self.price_labels = [None] * self.menu_max
+
+        for i, image_path in enumerate([item for item in os.listdir(self.path) if item.endswith('.png')]):
+            self.images[i] = PhotoImage(file=os.path.join(self.path, image_path))
+
+        ## -- Menu Item --
+        for i in range(self.menu_max):
+            self.borders[i] = LabelFrame(self.mainframe, bd=6, bg="white")
+            self.borders[i].grid(column=i%2, row=i//2, sticky=(N,W,E,S))
+            self.buttons[i] = tk.Button(self.borders[i], image=self.images[i])
+            self.buttons[i].grid(row=0, column=0, sticky=(E,W), columnspan=2)
+            self.food_labels[i] = Label(self.borders[i], text=f"Food {i}", font=("Courier", 12))
+            self.food_labels[i].grid(row=1, column=0, sticky=(E,W))
+            self.price_labels[i] = Label(self.borders[i], text=f"${i}", font=("Courier", 12))
+            self.price_labels[i].grid(row=1, column=1, sticky=(E,W))
+            self.desc_labels[i] = Label(self.borders[i], text =f"Desc {i}", font=("Arial", 10))
+            self.desc_labels[i].grid(row=2, column=0, sticky=(E,W), columnspan=2)
+
+
+    def binding_events(self, root):
+        root.bind(0, lambda event: self.keypress_handler(event))
+        root.bind(1, lambda event: self.keypress_handler(event))
+        root.bind(2, lambda event: self.keypress_handler(event))
+        root.bind(3, lambda event: self.keypress_handler(event))
+        root.bind(4, lambda event: self.keypress_handler(event))
+
+    def keypress_handler(self, event):
+        if event.char == '1': self.order()
+        elif event.char == '2': self.scroll_selection()
+
+    def scroll_selection(self):
+        self.selection_counter = (self.selection_counter + 1) % self.menu_max
+        for i in range(self.menu_max):
+            self.borders[i].config(bg='blue' if i==self.selection_counter else 'white')
+
+    def order(self):
+        print(f"Ordering {self.selection_counter}")
+
 
 root = Tk()
-# root.geometry('300x100')
-mainframe = Frame(root)
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.pack(expand=True, fill='both', anchor='center')
-
-def push(i):
-    print(path+os.listdir(path)[i])
-    if i==0:
-        border_0.config(bg='red' if border_state[0] else 'green')
-        border_state[0] = not border_state[0]
-    elif i==1:
-        border_1.config(bg='red' if border_state[1] else 'green')
-        border_state[1] = not border_state[1]
-
-def selection(event):
-    print(event)
-
-images = [None] * 2
-border_state = [True] * 2
-
-for i, image_path in enumerate([item for item in os.listdir(path) if item.endswith('.png')]):
-    images[i] = PhotoImage(file=os.path.join(path, image_path))
-border_0 = LabelFrame(mainframe, bd = 6, bg = "black")
-border_0.grid(column=0, row=0, sticky=(N, W, E, S))
-button_0 = tk.Button(border_0, command=lambda:push(0), image=images[0])
-button_0.grid(row=1, column=0, sticky=(E,W), columnspan=2)
-food_label_0 = Label(border_0, text = "Philip Lee", font =("Courier", 14))
-food_label_0.grid(row=2, column=0, sticky=(E,W))
-desc_label_1 = Label(border_0, text = "Food for Bunny", font =("Ariel", 12))
-desc_label_1.grid(row=3, column=0, sticky=(E,W), columnspan=2)
-price_label_2 = Label(border_0, text = "$4.50", font =("Courier", 14))
-price_label_2.grid(row=2, column=1, sticky=(E,W))
-
-border_1 = LabelFrame(mainframe, bd = 6, bg = "black")
-border_1.grid(column=1, row=0, sticky=(N, W, E, S))
-button_1 = tk.Button(border_1, command=lambda:push(1), image=images[1])
-button_1.grid(row=1, column=0, sticky=(E,W))
-food_label_1 = Label(border_1, text = "Jane Lee", font =("Courier", 14))
-food_label_1.grid(row=2, column=0, sticky=(E,W))
-desc_label_1 = Label(border_1, text = "The Bunny", font =("Ariel", 12))
-desc_label_1.grid(row=3, column=0, sticky=(E,W), columnspan=2)
-price_label_1 = Label(border_1, text = "$4.51", font =("Courier", 14))
-price_label_1.grid(row=2, column=1, sticky=(E,W))
-
-root.bind(0, lambda event: selection(event))
-root.bind(1, lambda event: selection(event))
-
+Menu(root)
 root.mainloop()
